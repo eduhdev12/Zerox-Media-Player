213877e18f21f9cc83224ea251ce75aaf2c45c1c
eduhdev12 <60825949+eduhdev12@users.noreply.github.com>
2020-06-21 21:30:32 +0300

    adding the source

---
+++ server.js
@@ -0,0 +1,45 @@
+// const http = require('http')
+// const fs = require('fs')
+// const config = require("./config.json")
+
+// const server = http.createServer(function(req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html'})
+//     fs.readFile('index.html', function(error, data) {
+//         if(error)
+//         {
+//             res.writeHead(404)
+//             res.write('Error page not found!')
+//         }
+//         else{
+//             res.write(data)
+//         }
+//         res.end()
+//     })
+
+// })
+
+// server.listen(config.port, function(error){
+//     if(error) {
+//         console.log('Error on the server!')
+//     }
+//     else{
+//         console.log(`Server is listening on localhost:${config.port}`)
+//     }
+// })
+const express = require('express')
+const ffmpeg = require('ffmpeg')
+const app = express()
+const config = require('./config.json')
+
+app.set('view-engine', 'ejs')
+
+app.get('/', (req, res) =>{
+    res.render('index.ejs')
+})
+
+app.get('/test', (req,res)=>{
+    res.render('index.ejs')
+    console.log('cacatr')
+})
+
+app.listen(config.port)
